c814ff612b820ded421981c4a322ac741c311d25
Darren Wayn <darrenwayn98@gmail.com>
2022-06-30 16:11:06 +0700

    added js

--- script.js
+++ script.js
@@ -10,12 +10,37 @@
 // document.querySelector('.guess').value = 23
 // console.log(document.querySelector('.guess').value)
 
-document.querySelector('.check').addEventListener('click', function() {
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+let score = 20
+document.querySelector('.number').textContent = secretNumber
+
+document.querySelector('.check').addEventListener
+('click', function() {
     const guess = document.querySelector('.guess').value
     console.table(guess, typeof guess)
 
     if (!guess) {
         console.log(document.querySelector('.message').textContent = 'No Number')
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'This Correct Number!'
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'To high!'
+            score--
+            document.querySelector('.score').textContent = score
+        } else {
+            document.querySelector('.message').textContent = 'You Lost The Game!'
+            document.querySelector('.message').textContent = 0
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'To Low!'
+            score--
+            document.querySelector('.score').textContent = score
+        } else {
+            document.querySelector('.message').textContent = 'You Lost The Game!'
+            document.querySelector('.message').textContent = 0
+        }
     }
 })
 
